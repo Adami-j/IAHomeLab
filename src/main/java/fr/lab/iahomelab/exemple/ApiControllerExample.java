3ac1bdebe298aa129fd774701fcb54f245ece877
Adami-j <adamij@3il.fr>
2026-08-18 13:52:31 +0200

    fix example controller to include test endpoint

--- src/main/java/fr/lab/iahomelab/exemple/ApiControllerExample.java
+++ src/main/java/fr/lab/iahomelab/exemple/ApiControllerExample.java
@@ -2,11 +2,17 @@ package fr.lab.iahomelab.exemple;
 
 import fr.lab.iahomelab.common.api.ApiPaths;
 import fr.lab.iahomelab.common.api.ApiV1Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @ApiV1Controller
 @RequestMapping(ApiPaths.API_V1 + "/exemple")
 public class ApiControllerExample {
 
+    @GetMapping("test")
+    public ResponseEntity<Integer> test() {
+        return ResponseEntity.status(300).build();
+    }
 
 }
