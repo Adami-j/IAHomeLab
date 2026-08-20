d1ad94f40ae1cc4ec956a9da3fb1322f51b5b9fb
Adami-j <71751140+Adami-j@users.noreply.github.com>
2026-08-20 16:06:08 +0200

    feat(setup): wire connection service repositories

--- src/main/java/fr/lab/iahomelab/setup/service/ConnectionService.java
+++ src/main/java/fr/lab/iahomelab/setup/service/ConnectionService.java
@@ -1,6 +1,8 @@
 package fr.lab.iahomelab.setup.service;
 
+import fr.lab.iahomelab.setup.repository.ComponentInstanceRepository;
 import fr.lab.iahomelab.setup.repository.ConnectionRepository;
+import fr.lab.iahomelab.setup.repository.SetupVersionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Service;
 public class ConnectionService {
 
     private final ConnectionRepository connectionRepository;
+    private final SetupVersionRepository setupVersionRepository;
+    private final ComponentInstanceRepository componentInstanceRepository;
 }
