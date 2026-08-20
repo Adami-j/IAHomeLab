f078cfbf976753eeccaac2099fa2a86a1690bf48
Adami-j <71751140+Adami-j@users.noreply.github.com>
2026-08-20 16:05:56 +0200

    feat(setup): wire component service repositories

--- src/main/java/fr/lab/iahomelab/setup/service/ComponentInstanceService.java
+++ src/main/java/fr/lab/iahomelab/setup/service/ComponentInstanceService.java
@@ -1,6 +1,7 @@
 package fr.lab.iahomelab.setup.service;
 
 import fr.lab.iahomelab.setup.repository.ComponentInstanceRepository;
+import fr.lab.iahomelab.setup.repository.SetupVersionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Service;
 public class ComponentInstanceService {
 
     private final ComponentInstanceRepository componentInstanceRepository;
+    private final SetupVersionRepository setupVersionRepository;
 }
