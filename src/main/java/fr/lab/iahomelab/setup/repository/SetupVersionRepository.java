7bc94d0e91517df4f5fa12147299fecdb1ccf6f7
Adami-j <71751140+Adami-j@users.noreply.github.com>
2026-08-20 19:42:41 +0200

    feat(setup): query frozen setup versions

--- src/main/java/fr/lab/iahomelab/setup/repository/SetupVersionRepository.java
+++ src/main/java/fr/lab/iahomelab/setup/repository/SetupVersionRepository.java
@@ -1,9 +1,12 @@
 package fr.lab.iahomelab.setup.repository;
 
 import fr.lab.iahomelab.setup.entity.SetupVersion;
+import fr.lab.iahomelab.setup.entity.SetupVersionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface SetupVersionRepository extends JpaRepository<SetupVersion, UUID> {
+
+    boolean existsBySetupIdAndStatus(UUID setupId, SetupVersionStatus status);
 }
